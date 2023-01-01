64868835ecda5926b7e7b06feb060fe1bcb47072
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2023-01-01 12:23:07 +0100

    delete files

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyKdjvOcCX4tWMvRP_7DwKaRLOkrkdM7U",
+  authDomain: "netflix-clone-2910e.firebaseapp.com",
+  projectId: "netflix-clone-2910e",
+  storageBucket: "netflix-clone-2910e.appspot.com",
+  messagingSenderId: "694970347813",
+  appId: "1:694970347813:web:51519a1201c72de1aeeacc",
+  measurementId: "G-1BCVGF2TNG",
+};
