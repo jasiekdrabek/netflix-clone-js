698063da1249ce9c864bde91d50e1d7bc42e439f
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2023-01-02 18:03:40 +0100

    change requests and image size

--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
+import shuffleArray from "./shuffleArray";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
-  const baseUrl = "https://image.tmdb.org/t/p/original/";
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(fetchUrl);
-      var results = request.data.results;
+      if (Array.isArray(fetchUrl)) {
+        var results = [];
+        var request;
+        for (let i = 0; i < fetchUrl.length; i++) {
+          request = await axios.get(fetchUrl[i]);
+          results = results.concat(request.data.results);
+        }
+      }else{
+        request = await axios.get(fetchUrl);
+        results = request.data.results;
+      }
+      results = shuffleArray(results);
       setMovies(results);
-      return request;
     }
     fetchData();
   }, [fetchUrl]);
@@ -18,24 +27,27 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {movies.map((movie) => (
+        {movies.map(
+          (movie) =>
             // @ts-ignore
-            ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path))&&(
-          <img
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            // @ts-ignore
-            key={movie.id}
-            src={`${baseUrl}${
-              isLargeRow
-                ? // @ts-ignore
-                  movie.poster_path
-                : // @ts-ignore
-                  movie.backdrop_path
-            }`}
-            alt=""
-          />
-          )
-        ))}
+            ((isLargeRow && movie.poster_path) ||
+              // @ts-ignore
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                // @ts-ignore
+                key={movie.id}
+                src={
+                  isLargeRow
+                    ? // @ts-ignore
+                      `https://image.tmdb.org/t/p/w342/` + movie.poster_path
+                    : // @ts-ignore
+                      `https://image.tmdb.org/t/p/w300/` + movie.backdrop_path
+                }
+                alt=""
+              />
+            )
+        )}
       </div>
     </div>
   );
