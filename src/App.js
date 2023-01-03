eb3f153652c75f2cd6d949eea167f2a38e8ace9f
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2023-01-03 17:42:49 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MoviesScreen from "./screens/MoviesScreen";
+import TvSeriesScreen from "./screens/TvSeriesScreen";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Routes>
+          <Route path="/tv" element={<TvSeriesScreen />} />
+          <Route path="/movie" element={<MoviesScreen />} />
+          <Route path="" element={<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
