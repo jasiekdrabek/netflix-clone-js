ecc2a27bd4ed7e1848158546e2665e6e0931a3f1
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2023-01-03 12:22:19 +0100

    Update Requests.js

--- src/Requests.js
+++ src/Requests.js
@@ -4,8 +4,8 @@ const requests = {
   fetchTrending: [`/discover/tv?api_key=${API_KEY}&language=en-US&with_watch_providers=8&watch_region=US&sort_by=popularity.des`,
   `/discover/movie?api_key=${API_KEY}&language=en-US&with_watch_providers=8&watch_region=US&sort_by=popularity.des`],
   fetchNetflixOriginals: [
-    `/discover/tv?api_key=${API_KEY}&witg_network=213`,
-    `/discover/movie?api_key=${API_KEY}&witg_network=213`,
+    `/discover/tv?api_key=${API_KEY}&witg_network=213&with_watch_providers=8&watch_region=US`,
+    `/discover/movie?api_key=${API_KEY}&witg_network=213&with_watch_providers=8&watch_region=US`,
   ],
   fetchTopRated: [
     `/discover/movie?api_key=${API_KEY}&language=en-US&with_watch_providers=8&watch_region=US&sort_by=vote_average.desc&vote_count.gte=200`,
