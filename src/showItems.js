dc02ede5a136e592d7378a8b24614b2a826c5557
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2023-01-03 17:40:59 +0100

    Create showItems.js

---
+++ src/showItems.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Row from "./Row";
+
+const showItems = (shuffleRequestGenres, records) => {
+  var items = [];
+  for (var i = 0; i < Math.min(records, shuffleRequestGenres.length); i++) {
+    items.push(
+      <Row
+        key={i}
+        title={shuffleRequestGenres[i].Name}
+        fetchUrl={shuffleRequestGenres[i].Url}
+      />
+    );
+  }
+  return items;
+};
+
+export default showItems;
