091a73ab102b91537779c1132de17b06583faf30
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2023-01-09 21:19:46 +0100

    Update showItems.js

--- src/functions/showItems.js
+++ src/functions/showItems.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Row from "../components/Row";
 
-const showItems = (shuffleRequestGenres, records) => {
+const showItems = (shuffleRequestGenres, records, type) => {
   var items = [];
   for (var i = 0; i < Math.min(records, shuffleRequestGenres.length); i++) {
     items.push(
       <Row
         key={i}
-        title={shuffleRequestGenres[i].Name}
-        fetchUrl={shuffleRequestGenres[i].Url}
+        media_type={type}
+        title={shuffleRequestGenres[i].name}
+        genreId={shuffleRequestGenres[i].genreId}
+        displayGenres={true}
       />
     );
   }
