d5375e66d1d2b79d27159219b3d64240764dd185
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2023-01-09 21:22:23 +0100

    searchbar profile and my list

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
+import { selectAvatar } from "../functions/userSlice";
 import "./Nav.css";
 
 function Nav() {
   const navigate = useNavigate();
+  const searchRef = useRef(null);
   const [show, handleShow] = useState(false);
+  const avatar = useSelector(selectAvatar);
+
   const goToTop = () => {
     window.scrollTo({
       top: 0,
     });
   };
+
   const transitonNavBar = () => {
     if (window.scrollY > 100) {
       handleShow(true);
@@ -18,6 +24,7 @@ function Nav() {
       handleShow(false);
     }
   };
+
   useEffect(() => {
     window.addEventListener("scroll", transitonNavBar);
 
@@ -30,6 +37,7 @@ function Nav() {
     e.preventDefault();
     auth.signOut();
   };
+
   function logoClick() {
     goToTop();
     navigate("/");
@@ -45,33 +53,65 @@ function Nav() {
     navigate("/tv");
   }
 
+  function profileClicik() {
+    goToTop();
+    navigate("/profile");
+  }
+
+  function myListClick() {
+    goToTop();
+    navigate("/myList");
+  }
+
+  const searchClick = (e) => {
+    e.preventDefault();
+    goToTop();
+    // @ts-ignore
+    navigate("/search", { state: searchRef.current?.value });
+  };
+
   return (
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__content">
         <img
           onClick={logoClick}
           className="nav__logo nav__element"
-          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          src={
+            "https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          }
           alt=""
         />
-        <h2 className="nav__element" onClick={movieClick}>
+        <h2 className="nav__element nav__h2" onClick={movieClick}>
           MOVIES
         </h2>
-        <h2 className="nav__element" onClick={tvClick}>
+        <h2 className="nav__element nav__h2" onClick={tvClick}>
           TV SERIES
         </h2>
+        <form className="nav__element">
+          <input
+            ref={searchRef}
+            placeholder="search"
+            className="nav__searchbar"
+          ></input>
+          <button className="nav__dropdownbutton" onClick={searchClick}>
+            Search
+          </button>
+        </form>
         <div className="nav__dropdown">
           <img
             className="nav__avatar nav__element"
-            src="https://i.pinimg.com/564x/19/b9/dd/19b9dde17c93c1dfe153e5c8ed7be575.jpg"
+            src={avatar?.avatar}
             alt=""
           />
           <div className="nav__dropdownContent">
+            <button className="nav__dropdownbutton" onClick={profileClicik}>
+              Profile
+            </button>
+            <button className="nav__dropdownbutton" onClick={myListClick}>
+              My list
+            </button>
             <button className="nav__dropdownbutton" onClick={logout}>
               Sign Out
-            </button>
-            <button className="nav__dropdownbutton">
-              Testbdgxbsfgndhjdthdndh
             </button>
           </div>
         </div>
