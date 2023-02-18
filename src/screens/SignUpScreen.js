8617757c92fa61ec0609788a609221821bda8375
Jan Drabek <jasiekdrabek@gmail.com>
2023-02-18 11:48:35 +0100

    Change SignUp/SignIn text

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 function SignUpScreen() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
-  const [signIn, setSignIn] = useState(true) 
+  const [signIn, setSignIn] = useState(true);
   const navigate = useNavigate();
 
   const register = (e) => {
@@ -29,9 +29,12 @@ function SignUpScreen() {
         db.collection("User watch provider")
           .doc(auth.currentUser?.uid)
           .set({ watchProviderId: "8", watchProviderName: "Netflix" });
-          db.collection("User avatar")
+        db.collection("User avatar")
           .doc(auth.currentUser?.uid)
-          .set({ avatar:"https://i.pinimg.com/564x/de/ec/1c/deec1c546541321a20403ad9cb5f5390.jpg"});
+          .set({
+            avatar:
+              "https://i.pinimg.com/564x/de/ec/1c/deec1c546541321a20403ad9cb5f5390.jpg",
+          });
 
         navigate("/");
       })
@@ -51,29 +54,38 @@ function SignUpScreen() {
       .catch((error) => alert(error));
   };
 
-  const handleClick = (e) =>{
+  const handleClick = (e) => {
     e.preventDefault();
-    if(signIn){
+    if (signIn) {
       Login(e);
-    }else{
+    } else {
       register(e);
     }
-  }
+  };
 
   return (
     <div className="singUpScreen">
-      <h1 className="red">THIS IS NOT REAL NETFLIX ALL USERS DATA ARE STORED IN FIREBASE</h1>
+      <h1 className="red">
+        THIS IS NOT REAL NETFLIX ALL USERS DATA ARE STORED IN FIREBASE
+      </h1>
       <form>
-        <h1>{signIn ? 'Sign In' : 'Sign Up'}</h1>
+        <h1>{signIn ? "Sign In" : "Sign Up"}</h1>
         <input ref={emailRef} type="email" placeholder="Email"></input>
         <input ref={passwordRef} type="password" placeholder="Password"></input>
         <button type="submit" onClick={handleClick}>
           Submit
         </button>
         <h4>
-          {signIn ? <span className="signUpScreen__grey">New to Netflix?</span>:''}
-          <span className="signUpScreen__link" onClick= {()=> setSignIn(!signIn)}>
-            {signIn ? ' Sign Up now.': 'I already have an account'}            
+          {signIn ? (
+            <span className="signUpScreen__grey">New to Netflix?</span>
+          ) : (
+            <span className="signUpScreen__grey">Already have an account?</span>
+          )}
+          <span
+            className="signUpScreen__link"
+            onClick={() => setSignIn(!signIn)}
+          >
+            {signIn ? " Sign Up now." : "login"}
           </span>
         </h4>
       </form>
